451968a112999eff98a238c1c22048930bcd339b
Mabel Felix <mabelfelix@Mabels-MacBook-Pro.local>
2016-10-22 15:41:46 -0400

    pull 4 background

--- web-breakz/static/timer.js
+++ web-breakz/static/timer.js
@@ -1,33 +1,11 @@
-//var audio = new Audio('get-onto-the-dancefloor.wav');
-//var isPlaying = false;
-
-// function updateTime(){
-//     // var currentTime = new Date();
-//     // var hello = 'hi';
-//     // var hours = currentTime.getHours();
-//     // var minutes = currentTime.getMinutes();
-//     // var seconds = currentTime.getSeconds();
-//     //
-//     // var minutes_remaining = minutes.valueOf();
-//     //
-//     // if (!isPlaying && minutes == minutes + 5) {
-//     //     audio.play();
-//     //     isPlaying = true;
-//     //     setTimeout(function() { audio.pause(); isPlaying = false; }, 600000);
-//     // }
-//
-//
-//     document.querySelector('.clockdiv .valueh').innerHTML = hours;
-//     document.querySelector('.clockdiv .valuem').innerHTML = minutes;
-//     document.querySelector('.clockdiv .values').innerHTML = seconds;
-// }
-// setInterval(updateTime, 1000);
+var audio = new Audio('static/get-onto-the-dancefloor.wav');
 
 
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
+    audio.play();
+    var timer = duration, hours, minutes, seconds;
     setInterval(function () {
-        hours = parseInt(timer / (60 * 24), 10);
+        hours = 00;
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -46,7 +24,7 @@ function startTimer(duration, display) {
 }
 
 window.onload = function () {
-    var thirtyMinutes = 60 * 30,
+    var thirtyMinutes = 60 * 1,
         display = document.querySelector('#time');
     startTimer(thirtyMinutes, display);
 };
